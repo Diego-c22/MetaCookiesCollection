37ead668b48590bcea7790001158d4aa68273535
Diego <Diegocortes079@gmail.com>
2022-08-23 23:26:00 -0500

    Add test to public sale mint

--- test/UnitTesting.js
+++ test/UnitTesting.js
@@ -2,7 +2,15 @@ const {
   time,
   loadFixture,
 } = require("@nomicfoundation/hardhat-network-helpers")
-const { expect } = require('chai')
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const addresses = [
+  '0x4e34EeC85C800Fc61829b1b50Edb0a45f57BB632',
+  '0x33f99cC965Ea46A44eAc7d4fDAff91429Ee4E43a',
+  '0x31c6683a2f80B17c9576a39554505CBdCb80501e',
+  '0x1EF008Fe5bDEE78b1C2Aafaf60631f40e84B3374'
+]
 
 describe('MetaCookies Contract', () => {
   const deployContractFixture = async () => {
@@ -22,38 +30,32 @@ describe('MetaCookies Contract', () => {
     })
   })
 
+  describe('Public Sale', () => {
+    it('Should revert an error when total tokens needed exceeds max supply', async () => {
+      const { contract, wallet, walletTo } = await loadFixture(deployContractFixture)
+      const secondUser = contract.connect(walletTo)
+      await contract.activePublicSale(true);
+      const amount = ethers.utils.parseUnits('0.15', 'ether')
+      await secondUser.publicSaleMint(10, { value: amount})
+      await contract.airdrop(addresses, 830)
+      await expect(contract.publicSaleMint(10, { value: amount})).to.be.revertedWith('ERC721A: Amount of tokens exceeds max supply.');
+    })
+  })
+
   describe('Airdrop', () => {
     it('Should revert an error when is called by user different of the owner', async () => {
       const { contract, wallet, walletTo } = await loadFixture(deployContractFixture)
       const secondUser = contract.connect(walletTo)
-      const addresses = [
-        '0x4e34EeC85C800Fc61829b1b50Edb0a45f57BB632',
-        '0x33f99cC965Ea46A44eAc7d4fDAff91429Ee4E43a',
-        '0x31c6683a2f80B17c9576a39554505CBdCb80501e',
-        '0x1EF008Fe5bDEE78b1C2Aafaf60631f40e84B3374'
-      ]
       await expect(secondUser.airdrop(addresses, 5)).to.be.revertedWith('Ownable: caller is not the owner');
     })
 
     it('Should revert an error when total tokens needed exceeds max supply', async () => {
       const { contract, wallet, walletTo } = await loadFixture(deployContractFixture)
-      const addresses = [
-        '0x4e34EeC85C800Fc61829b1b50Edb0a45f57BB632',
-        '0x33f99cC965Ea46A44eAc7d4fDAff91429Ee4E43a',
-        '0x31c6683a2f80B17c9576a39554505CBdCb80501e',
-        '0x1EF008Fe5bDEE78b1C2Aafaf60631f40e84B3374'
-      ]
       await expect(contract.airdrop(addresses, 900)).to.be.revertedWith('ERC721A: Amount of tokens exceeds max supply.');
     })
 
     it('Should change all addresses balance.', async () => {
       const { contract, wallet, walletTo } = await loadFixture(deployContractFixture)
-      const addresses = [
-        '0x4e34EeC85C800Fc61829b1b50Edb0a45f57BB632',
-        '0x33f99cC965Ea46A44eAc7d4fDAff91429Ee4E43a',
-        '0x31c6683a2f80B17c9576a39554505CBdCb80501e',
-        '0x1EF008Fe5bDEE78b1C2Aafaf60631f40e84B3374'
-      ]
       await contract.airdrop(addresses, 600);
 
       const balances = await Promise.all(addresses.map(async i => (await contract.balanceOf(i)).toString()));
